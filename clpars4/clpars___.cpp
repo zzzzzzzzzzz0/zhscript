7a93544974f0a0224e36ae027cd597ed4af4977c
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2015-02-10 15:28:41 +0800

    命令行解析库：出现多匹配时排除b、e、c类型

--- clpars4/clpars___.cpp
+++ clpars4/clpars___.cpp
@@ -180,9 +180,19 @@ int clpars___::cb__(const char*flag,bool by_help,bool no,int& i1,int&i,
 					b=(ci->type_ == 'h');
 				} else {
 					if(!b){
-						list<string>::iterator si = find_if(ci->flags_.begin(), ci->flags_.end(),
-								comp___(flag,ci->type_,&src2,&arg1) );
-						b=(si != ci->flags_.end());
+						bool b2 = true;
+						switch(ci->type_) {
+						case 'b': case 'e': case 'c':
+							if(has > 0)
+								b2 = false;
+							break;
+						}
+						if(b2) {
+							list<string>::iterator si = find_if(
+									ci->flags_.begin(), ci->flags_.end(),
+									comp___(flag,ci->type_,&src2,&arg1) );
+							b=(si != ci->flags_.end());
+						}
 					}
 				}
 				break;
@@ -350,74 +360,4 @@ void clpars___::pause__(bool pause, int argc, char** argv) {
 		}
     }
 
-}
-
-dlle___ void del__(clpars___* clp){
-	if(clp)
-		delete clp;
-}
-
-dlle___ void one__(int*err,char*buf,void*ce,void* shangji,bool no,int argc,...){
-	clpars___ clp;
-    va_list argv;
-    va_start(argv, argc);
-    int sp;
-	clp.set__(buf,err,false,argc,argv,&sp);
-	if(!*err)
-		clp.par__(buf,err,ce,shangji,argc,argv,sp,no);
-	va_end(argv);
-}
-
-static clpars___ clpars_;
-
-dlle___ clpars___* set__(int*err,char*buf,bool need_new,bool add,int argc,...){
-	clpars___* clp;
-	if(need_new)
-		clp=new clpars___();
-	else
-		clp=&clpars_;
-    va_list argv;
-    va_start(argv, argc);
-    clp->set__(buf,err,add,argc,argv,NULL);
-	va_end(argv);
-	return clp;
-}
-
-dlle___ void par__(int* err,char*buf,void*ce,void* shangji,clpars___* clp,bool no,int argc,...){
-	if(!clp)
-		clp=&clpars_;
-    va_list argv;
-    va_start(argv, argc);
-    clp->par__(buf,err,ce,shangji,argc,argv,0,no);
-	va_end(argv);
-}
-
-dlle___ void info__(char**addr_ret,clpars___* clp,const char* t1,const char* t2,const char* n,bool yange){
-	if(!clp)
-		clp=&clpars_;
-	clp->info__(addr_ret,t1,t2,n,yange);
-}
-
-dlle___ void info3__(int* err,char*buf,void*ce,void* shangji,clpars___* clp,char* code){
-	if(!clp)
-		clp=&clpars_;
-	clp->info__(buf,err,ce,shangji,code);
-}
-
-dlle___ void pause__(clpars___* clp,bool pause,int argc,...){
-	if(!clp)
-		clp=&clpars_;
-	char** argv = NULL;
-	if(argc > 0) {
-		argv = new char*[argc];
-		va_list argv1;
-		va_start(argv1, argc);
-	    for (int i = 0; i < argc; ++i){
-	        argv[i] = va_arg(argv1, char*);
-	    }
-		va_end(argv1);
-	}
-	clp->pause__(pause, argc, argv);
-	if(argv)
-		delete argv;
 }
---
+++ clpars4/dlle.cpp
@@ -0,0 +1,79 @@
+/*
+ * dlle.cpp
+ *
+ *  Created on: 2015年1月11日
+ *      Author: zzzzzzzzzzz
+ */
+
+#include "clpars___.h"
+#include "../new_gg/def1.h"
+
+dlle___ void del__(clpars___* clp){
+	if(clp)
+		delete clp;
+}
+
+dlle___ void one__(int*err,char*buf,void*ce,void* shangji,bool no,int argc,...){
+	clpars___ clp;
+    va_list argv;
+    va_start(argv, argc);
+    int sp;
+	clp.set__(buf,err,false,argc,argv,&sp);
+	if(!*err)
+		clp.par__(buf,err,ce,shangji,argc,argv,sp,no);
+	va_end(argv);
+}
+
+static clpars___ clpars_;
+
+dlle___ clpars___* set__(int*err,char*buf,bool need_new,bool add,int argc,...){
+	clpars___* clp;
+	if(need_new)
+		clp=new clpars___();
+	else
+		clp=&clpars_;
+    va_list argv;
+    va_start(argv, argc);
+    clp->set__(buf,err,add,argc,argv,NULL);
+	va_end(argv);
+	return clp;
+}
+
+dlle___ void par__(int* err,char*buf,void*ce,void* shangji,clpars___* clp,bool no,int argc,...){
+	if(!clp)
+		clp=&clpars_;
+    va_list argv;
+    va_start(argv, argc);
+    clp->par__(buf,err,ce,shangji,argc,argv,0,no);
+	va_end(argv);
+}
+
+dlle___ void info__(char**addr_ret,clpars___* clp,const char* t1,const char* t2,const char* n,bool yange){
+	if(!clp)
+		clp=&clpars_;
+	clp->info__(addr_ret,t1,t2,n,yange);
+}
+
+dlle___ void info3__(int* err,char*buf,void*ce,void* shangji,clpars___* clp,char* code){
+	if(!clp)
+		clp=&clpars_;
+	clp->info__(buf,err,ce,shangji,code);
+}
+
+dlle___ void pause__(clpars___* clp,bool pause,int argc,...){
+	if(!clp)
+		clp=&clpars_;
+	char** argv = NULL;
+	if(argc > 0) {
+		argv = new char*[argc];
+		va_list argv1;
+		va_start(argv1, argc);
+	    for (int i = 0; i < argc; ++i){
+	        argv[i] = va_arg(argv1, char*);
+	    }
+		va_end(argv1);
+	}
+	clp->pause__(pause, argc, argv);
+	if(argv)
+		delete argv;
+}
