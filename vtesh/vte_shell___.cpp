2c4b109be8acbf74a322307d58caae866c514c5c
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2016-02-21 07:39:15 +0800

    梳理回调

--- vtesh/vte_shell___.cpp
+++ vtesh/vte_shell___.cpp
@@ -9,40 +9,6 @@
 #include "sh-base/extern.h"
 #include "sh-base/extern2.h"
 
-static s1___* new_view_s1_ = new s1___("新开", "", ' ');
-static s1___* contents_changed_s1_ = new s1___("内容改变", "contents-changed", 'v');
-static s1___* char_size_changed_s1_ = new s1___(NULL, "char-size-changed", 'v');
-static s1___* commit_s1_ = new s1___(NULL, "commit", 'v');
-static s1___* beep_s1_ = new s1___(NULL, "beep", 'v');
-static s1___* child_exited_s1_ = new s1___(NULL, "child-exited", 'v');
-static s1___* copy_clipboard_s1_ = new s1___(NULL, "copy-clipboard", 'v');
-static s1___* current_directory_uri_changed_s1_ = new s1___(NULL, "current-directory-uri-changed", 'v');
-static s1___* current_file_uri_changed_s1_ = new s1___(NULL, "current-file-uri-changed", 'v');
-static s1___* cursor_moved_s1_ = new s1___(NULL, "cursor-moved", 'v');
-static s1___* decrease_font_size_s1_ = new s1___(NULL, "decrease-font-size", 'v');
-static s1___* deiconify_window_s1_ = new s1___(NULL, "deiconify-window", 'v');
-static s1___* emulation_changed_s1_ = new s1___(NULL, "emulation-changed", 'v');
-static s1___* encoding_changed_s1_ = new s1___(NULL, "encoding-changed", 'v');
-static s1___* eof_s1_ = new s1___(NULL, "eof", 'v');
-static s1___* icon_title_changed_s1_ = new s1___("图标标题改变", "icon-title-changed", 'v');
-static s1___* iconify_window_s1_ = new s1___(NULL, "iconify-window", 'v');
-static s1___* increase_font_size_s1_ = new s1___(NULL, "increase-font-size", 'v');
-static s1___* lower_window_s1_ = new s1___(NULL, "lower-window", 'v');
-static s1___* maximize_window_s1_ = new s1___(NULL, "maximize-window", 'v');
-static s1___* move_window_s1_ = new s1___(NULL, "move-window", 'v');
-static s1___* paste_clipboard_s1_ = new s1___(NULL, "paste-clipboard", 'v');
-static s1___* raise_window_s1_ = new s1___(NULL, "raise-window", 'v');
-static s1___* refresh_window_s1_ = new s1___(NULL, "refresh-window", 'v');
-static s1___* resize_window_s1_ = new s1___(NULL, "resize-window", 'v');
-static s1___* restore_window_s1_ = new s1___(NULL, "restore-window", 'v');
-static s1___* selection_changed_s1_ = new s1___(NULL, "selection-changed", 'v');
-static s1___* status_line_changed_s1_ = new s1___(NULL, "status-line-changed", 'v');
-static s1___* text_deleted_s1_ = new s1___(NULL, "text-deleted", 'v');
-static s1___* text_inserted_s1_ = new s1___(NULL, "text-inserted", 'v');
-static s1___* text_modified_s1_ = new s1___(NULL, "text-modified", 'v');
-static s1___* text_scrolled_s1_ = new s1___(NULL, "text-scrolled", 'v');
-static s1___* window_title_changed_s1_ = new s1___(NULL, "window-title-changed", 'v');
-
 static window___* window__(vte_view___* v) {
 	return (window___*)v->window__();
 }
@@ -50,7 +16,7 @@ static window___* window__(vte_view___* v) {
 static glong col_old_ = 0, row_old_ = 0;
 static string outext_;
 
-static void cb__(VteTerminal* vte, void* data_in) {
+static void call4__(VteTerminal* vte, void* data_in, s1___* contents_changed_s1 = NULL) {
 	vte_view___* v = (vte_view___*)vte_view___::from__(vte);
 	s1___* s1 = NULL;
 
@@ -62,11 +28,11 @@ static void cb__(VteTerminal* vte, void* data_in) {
 		}
 	}
 	if(!s1) {
-		printf("{cb! %p %p}\n",v,data_in);
+		printf("{cb! %p %p %p}\n",v,vte,data_in);
 		return;
 	}
 
-	if(s1 == contents_changed_s1_) {
+	if(s1 == contents_changed_s1) {
 		glong col, row;
 		vte_terminal_get_cursor_position(v->handle__(), &col, &row);
 		gchar *vte_text = vte_terminal_get_text_range(v->handle__(),
@@ -83,6 +49,54 @@ static void cb__(VteTerminal* vte, void* data_in) {
 		col_old_ = col;
 	}
 	call4__(vte_view___::scrolled_from__(vte), window__(v), s1, 0);
+}
+
+static void cb2__(VteTerminal* vte, int arg1, void* data_in) {
+	call4__(vte, data_in);
+}
+static void cb3__(VteTerminal* vte, int arg1, int arg2, void* data_in) {
+	call4__(vte, data_in);
+}
+static void cb3_2__(VteTerminal* vte, char* arg1, int arg2, void* data_in) {
+	call4__(vte, data_in);
+}
+
+static s1___* new_view_s1_ = new s1___("新开", "", ' ');
+static s1___* contents_changed_s1_ = new s1___("内容改变", "contents-changed", 'v');
+//static s1___* char_size_changed_s1_ = new s1___(NULL, "char-size-changed", 'v', G_CALLBACK(cb3__));
+//static s1___* commit_s1_ = new s1___(NULL, "commit", 'v', G_CALLBACK(cb3_2__));
+static s1___* beep_s1_ = new s1___(NULL, "beep", 'v');
+static s1___* child_exited_s1_ = new s1___(NULL, "child-exited", 'v'/*, G_CALLBACK(cb2__)*/);
+static s1___* copy_clipboard_s1_ = new s1___(NULL, "copy-clipboard", 'v');
+static s1___* current_directory_uri_changed_s1_ = new s1___(NULL, "current-directory-uri-changed", 'v');
+static s1___* current_file_uri_changed_s1_ = new s1___(NULL, "current-file-uri-changed", 'v');
+static s1___* cursor_moved_s1_ = new s1___(NULL, "cursor-moved", 'v');
+static s1___* decrease_font_size_s1_ = new s1___(NULL, "decrease-font-size", 'v');
+static s1___* deiconify_window_s1_ = new s1___(NULL, "deiconify-window", 'v');
+static s1___* emulation_changed_s1_ = new s1___(NULL, "emulation-changed", 'v');
+static s1___* encoding_changed_s1_ = new s1___(NULL, "encoding-changed", 'v');
+static s1___* eof_s1_ = new s1___(NULL, "eof", 'v');
+static s1___* icon_title_changed_s1_ = new s1___("图标标题改变", "icon-title-changed", 'v');
+static s1___* iconify_window_s1_ = new s1___(NULL, "iconify-window", 'v');
+static s1___* increase_font_size_s1_ = new s1___(NULL, "increase-font-size", 'v');
+static s1___* lower_window_s1_ = new s1___(NULL, "lower-window", 'v');
+static s1___* maximize_window_s1_ = new s1___(NULL, "maximize-window", 'v');
+static s1___* move_window_s1_ = new s1___(NULL, "move-window", 'v', G_CALLBACK(cb3__));
+static s1___* paste_clipboard_s1_ = new s1___(NULL, "paste-clipboard", 'v');
+static s1___* raise_window_s1_ = new s1___(NULL, "raise-window", 'v');
+static s1___* refresh_window_s1_ = new s1___(NULL, "refresh-window", 'v');
+static s1___* resize_window_s1_ = new s1___(NULL, "resize-window", 'v', G_CALLBACK(cb3__));
+static s1___* restore_window_s1_ = new s1___(NULL, "restore-window", 'v');
+static s1___* selection_changed_s1_ = new s1___(NULL, "selection-changed", 'v');
+static s1___* status_line_changed_s1_ = new s1___(NULL, "status-line-changed", 'v');
+static s1___* text_deleted_s1_ = new s1___(NULL, "text-deleted", 'v');
+static s1___* text_inserted_s1_ = new s1___(NULL, "text-inserted", 'v');
+static s1___* text_modified_s1_ = new s1___(NULL, "text-modified", 'v');
+static s1___* text_scrolled_s1_ = new s1___(NULL, "text-scrolled", 'v', G_CALLBACK(cb2__));
+static s1___* window_title_changed_s1_ = new s1___(NULL, "window-title-changed", 'v');
+
+static void cb__(VteTerminal* vte, void* data_in) {
+	call4__(vte, data_in, contents_changed_s1_);
 }
 
 view___* vte_shell___::new_view__(GtkWidget* scrolled2, window___* window) {
@@ -154,22 +168,25 @@ bool vte_shell___::api__(void* shangji, void* ce, deque<string>* p, char* buf, l
 			col_old_ = row_old_ = 0;
 			return true;
 		}
-		if(p1 == "") {
-			string s;
-			for(size_t i = 2; i < p->size(); i++)
-				s += (*p)[i];
-			* addr_ret = dup__(s.c_str());
-			return true;
-		}
 		if(p1 == "z") {
 			v=(vte_view___*)get_view__(p0,page_num,p1);if(!v)return true;
-			//printf("%s\n",vte_terminal_get_status_line (v->vte__()));
-			//printf("%s\n",vte_terminal_get_window_title (v->vte__()));
+			//printf("%s\n",vte_get_features());
+			//printf("%s\n",vte_terminal_get_status_line (v->handle__()));
+			//printf("%s\n",vte_terminal_get_window_title (v->handle__()));
 
-			/*vte_terminal_reset(v->vte__(), false, false);
-			gchar *vte_text = vte_terminal_get_text (v->vte__(), NULL, NULL, NULL);
+			/*vte_terminal_reset(v->handle__(), false, false);
+			gchar *vte_text = vte_terminal_get_text (v->handle__(), NULL, NULL, NULL);
 			printf("%s",vte_text);
 			g_free (vte_text);*/
+
+			/*if(vte_terminal_search_get_regex (v->handle__()) == NULL)
+				vte_terminal_search_set_regex (v->handle__(),
+						vte_regex_new ("z", 1, 0//guint32 flags
+								, NULL//GError **error
+								),
+						0//guint32 flags
+						);*/
+			vte_terminal_search_find_next (v->handle__());
 			return true;
 		}
 		if(p1 == "背景色" || p1 == "前景色") {
@@ -246,7 +263,7 @@ vte_shell___::vte_shell___():shell___() {
 	GCallback cb = G_CALLBACK(cb__);
 	for(size_t i = 0; i < s1s_length__(); i++) {
 		s1___* s1 = s1s__(i);
-		if(s1->type__() == 'v') {
+		if(s1->type__() == 'v' && !s1->cb_) {
 			s1->cb_ = cb;
 		}
 	}
