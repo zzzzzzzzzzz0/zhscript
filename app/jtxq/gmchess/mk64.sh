5f5c415743cdda550b7a0cfa6d42e55282fc338b
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2016-02-21 01:59:59 +0800

    改

--- app/jtxq/gmchess/mk64.sh
+++ app/jtxq/gmchess/mk64.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
-g++ src/*.cc src/*.cpp `pkg-config gtkmm-2.4 --libs --cflags` -shared -o"libgmchessengine.so.64" -fPIC -O3 -Wall -fmessage-length=0
+#!/bin/bash
+z=".64"
+if [ "$1" != "" ]
+then
+	z="$1"
+fi
+g++ src/*.cc src/*.cpp `pkg-config gtkmm-2.4 --libs --cflags` -shared -o"libgmchessengine.so$z" -fPIC -O3 -Wall -fmessage-length=0
 
