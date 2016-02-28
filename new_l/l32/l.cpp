18d82ad46b00bf32d69c71e6fb9542bd60c6bb1b
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2016-02-28 15:37:11 +0800

    让可以找更多启动文件

--- new_l/l32/l.cpp
+++ new_l/l32/l.cpp
@@ -1,26 +1,28 @@
 #include "l4/args_util.cc"
 #include "l4/l4___.cc"
 
-bool file__(const char* path,const char* ext,const char* sp,string& cmdline){
-	string file;
-	file=path;
-	file+=ext;
-	//if((access(file.c_str(),F_OK))==0){
-	struct stat info;
-	if(lstat(file.c_str(), &info) == 0){
+static bool file__(const char* path,const char* ext,const char* sp,string& cmdline){
+	if(lstat__(path, ext)){
 		cmdline+=sp;
-		cmdline+=file;
+		cmdline+=path;
+		cmdline+=ext;
 		return true;
 	}
 	return false;
 }
-bool file__(const char* path,string& cmdline){
+static bool file__(const char* path,string& cmdline){
 	int i=0;
 	if(file__(path,".ini"," -I ",cmdline))
 		i++;
 	if(file__(path,".zs"," ",cmdline))
 		i++;
 	return i>0;
+}
+
+static bool get_lnk_ok__(const char* path) {
+	if(lstat__(path, ".zs"))
+		return true;
+	return false;
 }
 
 static l4___ l4_;
@@ -33,7 +35,7 @@ int main(int argc,char** argv,char** env) {
 		if(which__(path,buf))
 			path=buf.c_str();
 		string cmdline="-S syn-zh -E err-zh";
-		if(!file__(path,cmdline)&&get_lnk__(path,buf,true)){
+		if(!file__(path,cmdline)&&get_lnk__(path,buf,true, get_lnk_ok__)){
 			path=buf.c_str();
 			file__(path,cmdline);
 		}
