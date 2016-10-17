94e4c994e3aa56a7c86c1fbcd0ac287ed4c0b19d
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2016-10-18 02:45:29 +0800

    sh-base：增“隐藏标签”、“隐藏标签前”、“菜单标签”

--- sh-base/gtk/sh-base/api_page.cpp
+++ sh-base/gtk/sh-base/api_page.cpp
@@ -13,6 +13,7 @@ bool shell___::api_page__(void*shangji,void*ce,deque<string>* p,char*buf,long si
 	const string& p1=(*p)[1];
 	window___* w;
 	int page_num;
+	char z = 0;
 
 	if(p1=="标签号"){
 		w=get_window__(p0,page_num,p1);if(!w)return true;
@@ -44,18 +45,52 @@ bool shell___::api_page__(void*shangji,void*ce,deque<string>* p,char*buf,long si
 		l2s__(w->c__()->n_pages__(), buf);
 		return true;
 	}
-	if(p1=="标签" || p1=="标签前"){
+	if(p1=="标签")
+		z = 't';
+	else if(p1=="标签前")
+		z = 'b';
+	else if(p1=="隐藏标签")
+		z = 'h';
+	else if(p1=="隐藏标签前")
+		z = 'i';
+	else if(p1=="菜单标签")
+		z = 'm';
+	switch(z) {
+	case 't': case 'b': case 'h': case 'i': case 'm':
+	{
 		w=get_window__(p0,page_num,p1);if(!w)return true;
-		GtkLabel* l= p1=="标签" ? w->c__()->label__(page_num) :  w->c__()->label2__(page_num);
+		GtkLabel* l;
+		switch(z) {
+		case 't': case 'h':
+			l = w->c__()->label__(page_num);
+			break;
+		default:
+			l = w->c__()->label2__(page_num);
+			break;
+		case 'm':
+			l=w->c__()->menu_label__(page_num);
+			break;
+		}
 		if(!l){
 			err_wufa__(p1,p0.c_str());
 			return true;
 		}
-		if(p->size()<3){
-			cpy__(buf, gtk_label_get_text(l), siz);
-		}else
-			gtk_label_set_markup(l, (*p)[2].c_str());
+		switch(z) {
+		case 'i': case 'h':
+			gtk_widget_hide (GTK_WIDGET(l));
+			break;
+		default:
+			if(p->size()<3){
+				cpy__(buf, gtk_label_get_text(l), siz);
+			}else {
+				gtk_widget_show(GTK_WIDGET(l));
+				const string& s = (*p)[2];
+				gtk_label_set_markup(l, s.c_str());
+			}
+			break;
+		}
 		return true;
+	}
 	}
 	if(p1=="标签提示"){
 		w=get_window__(p0,page_num,p1);if(!w)return true;
