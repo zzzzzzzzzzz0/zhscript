da6d28ca5ebfdfc20879b5afa950e7a0c8465001
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2019-01-25 12:54:32 +0800

    “行输入处理”支持跳出、再来，改去尾

--- clpars4/gets.cpp
+++ clpars4/gets.cpp
@@ -5,10 +5,8 @@
  *      Author: zzzzzzzzzzz
  */
 #include <stdio.h>
-#include "../new_gg/def1.h"
-
-extern void* jsq_;
-extern callback3_2___ cb2_;
+#include <string.h>
+#include "pub.h"
 
 dlle___ void gets__(int*err, char*buf, long bufsiz,void*ce, void*shangji, const char*code) {
 	char* ret;
@@ -17,15 +15,24 @@ dlle___ void gets__(int*err, char*buf, long bufsiz,void*ce, void*shangji, const 
 		ret = fgets(buf, bufsiz, stdin);
 		if(ret != buf)
 			break;
-		for(int i = 0; buf[i]; i++) {
-			switch(buf[i]) {
-			case '\n':
-			case '\r':
+		size_t i = strlen(buf);
+		while(i > 0) {
+			char c = buf[i];
+			if(c == '\n' || c == '\r')
 				buf[i] = 0;
+			else
 				break;
-			}
+			i--;
 		}
 		cb2_(jsq_,shangji,err,ce,code,false,NULL,1,argv2,0);
+		if(*err == jieshiqi_err_go_+keyword_continue_){
+			*err=0;
+			continue;
+		}
+		if(*err == jieshiqi_err_go_+keyword_break_){
+			*err=0;
+			break;
+		}
 		if(*err)
 			break;
 	}
