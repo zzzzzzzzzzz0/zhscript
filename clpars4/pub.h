ea04fce7b089ac0bee25dcbd78a0f1e2726c5859
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2019-01-25 12:50:47 +0800

    公

---
+++ clpars4/pub.h
@@ -0,0 +1,17 @@
+/*
+ * pub.h
+ *
+ *  Created on: 2019年1月25日
+ *      Author: zzzzzzzzzzz
+ */
+
+#ifndef PUB_H_
+#define PUB_H_
+
+#include "../new_gg/def1.h"
+#include "../new_gg/l4/keyword.h"
+
+extern void* jsq_;
+extern callback3_2___ cb2_;
+
+#endif /* PUB_H_ */
