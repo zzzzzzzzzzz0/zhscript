97ef76900709c4075c5a48aa70bfe3a2cff7bfd0
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2019-01-29 12:51:34 +0800

    格式

--- gjk4/int2fmtstr.c
+++ gjk4/int2fmtstr.c
@@ -11,52 +11,52 @@ _dle void int2fmtstr__(int* c4w4,char* buf,long siz,unsigned long src,int argc,.
 	int i1=(argc + 1)/2 - 1,i2,i3,ret_i;
 	_for_args( argc )
 		if((i % 2)==0){
-                  jinzhi_len++;
-                  i3=atoi(s);
-                  if(i3<1){
-                	  *c4w4=1;
-                	  strcpy(buf,s);
-                	  strcat(buf," lt 1");
-                	  return;
-                  }
-                  jinzhi[i1 - jinzhi_len]=i3;
+			jinzhi_len++;
+			i3=atoi(s);
+			if(i3<1){
+				*c4w4=1;
+				strcpy(buf,s);
+				strcat(buf," lt 1");
+				return;
+			}
+			jinzhi[i1 - jinzhi_len]=i3;
 		}else{
-                  fenge	[i1 - jinzhi_len]=s;
+			fenge [i1 - jinzhi_len]=s;
 		}
 	_next_args
 	if(++jinzhi_len<1)
 		jinzhi_len=1;
-    i1=jinzhi_len-1;
-    for(jinzh_i=jinzhi_len;jinzh_i<32;jinzh_i++){
-          jinzhi[jinzh_i]=jinzhi[i1];
-          fenge[jinzh_i]=fenge[i1];
-    }
+	i1=jinzhi_len-1;
+	for(jinzh_i=jinzhi_len;jinzh_i<32;jinzh_i++){
+		jinzhi[jinzh_i]=jinzhi[i1];
+		fenge[jinzh_i]=fenge[i1];
+	}
 	/*for(i1=0;i1<32;i1++)
 		printf("%d%s",jinzhi[i1],fenge[i1]);*/
-    ret_i=127;
-    ret[ret_i]=0;
+	ret_i=127;
+	ret[ret_i]=0;
 	for(jinzh_i=0;jinzh_i<32;jinzh_i++){
 		quan=jinzhi[jinzh_i];
 		shu=src%quan;
 		//printf("%lu %lu %lu %d %d\n",src,quan,shu,jinzh_i,jinzhi_len);
-        i1=sprintf__(buf,"%lu",shu);
-        ret_i-=i1;
-        for(i2=0;i2<i1;i2++)
-                  ret[ret_i+i2]=buf[i2];
-        src/=quan;
+		i1=sprintf__(buf,"%lu",shu);
+		ret_i-=i1;
+		for(i2=0;i2<i1;i2++)
+			ret[ret_i+i2]=buf[i2];
+		src/=quan;
 
-        if(src==0&&jinzh_i>=jinzhi_len-1)
-                  break;
+		if(src==0&&jinzh_i>=jinzhi_len-1)
+			break;
 
-        i3=jinzh_i+1;
-        if(i3>=32){
-                  strcpy(buf,"over");
-                  return;
+		i3=jinzh_i+1;
+		if(i3>=32){
+			strcpy(buf,"over");
+			return;
 		}
 		i1=strlen(fenge[i3]);
-        ret_i-=i1;
-        for(i2=0;i2<i1;i2++)
-                  ret[ret_i+i2]=fenge[i3][i2];
+		ret_i-=i1;
+		for(i2=0;i2<i1;i2++)
+			ret[ret_i+i2]=fenge[i3][i2];
 	}
 	strcpy(buf,ret+ret_i);
 }
@@ -98,9 +98,9 @@ _dle long fmtstr2int__(int* c4w4,char* buf,long siz,char* src,int argc,...){
 				}
 				//printf("shu=%ld\n",shu4);
 				for(jinzhi_i=wei-1;jinzhi_i>=0;jinzhi_i--){
-                                        jinzh_i=jinzhi_len-1-jinzhi_i;
-                                        if(jinzh_i<0)
-                                          jinzh_i=0;
+					jinzh_i=jinzhi_len-1-jinzhi_i;
+					if(jinzh_i<0)
+						jinzh_i=0;
 					shu4*=jinzhi[jinzh_i];
 					//printf("\t[%d]%d shu=%ld\n",jinzh_i,jinzhi[jinzh_i],shu4);
 				}
