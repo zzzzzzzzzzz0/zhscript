245436ad39fabe3b123d6975237a935842202bfa
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2019-12-25 20:13:09 +0800

    wnck4

--- wnck4/wnck.cpp
+++ wnck4/wnck.cpp
@@ -11,6 +11,8 @@
 #include "../new_gg/l4/keyword.h"
 
 #include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 static int s2i__(const char* s1,int i){
@@ -51,23 +53,24 @@ dlle___ void init__(callback2_2___ cb,void* jsq, void* main_qu, l4_err_out___ l4
 	l4_err_out_ = l4_err_out;
 }
 
-dlle___ void for__(int*err, void *ce, void* shangji,const char*code, const char*fmt,WnckScreen *screen){
+dlle___ void for__(int*err, void *ce, void* shangji,const char*code, bool use_aws, const char*fmt,WnckScreen *screen){
 	GList *window_l;
-	char p1[32], /*p2[] = {0, 0},*/ p3[32];
-	WnckWorkspace *aws = wnck_screen_get_active_workspace(screen);
+	char p1[32], p2[32];
+	WnckWorkspace *aws = NULL;
+	if(use_aws)
+		aws = wnck_screen_get_active_workspace(screen);
 	for (window_l = wnck_screen_get_windows (screen); window_l != NULL; window_l = window_l->next){
 		WnckWindow *window = WNCK_WINDOW (window_l->data);
 		sprintf(p1,fmt,(long)window);
-		//p2[0] = wnck_window_is_skip_tasklist(window) ? '0' : '1';
 
 		WnckWorkspace *ws = wnck_window_get_workspace(window);
 		int i = wnck_workspace_get_number(ws) + 1;
 		//wnck_window_is_on_workspace(window, aws)
-		if(ws == aws)
+		if(aws && ws == aws)
 			i = -i;
-		sprintf(p3, "%d", i);
+		sprintf(p2, "%d", i);
 
-		cb_(jsq_,shangji,err, ce,code,false, NULL, 2, p1, /*p2,*/ p3);
+		cb_(jsq_,shangji,err, ce,code,false, NULL, 3, p1, p2, wnck_window_is_skip_tasklist(window) ? "0" : "1");
 		if(*err){
 			if(*err==jieshiqi_err_go_+keyword_continue_){
 				*err=0;
@@ -124,24 +127,31 @@ dlle___ void window_set_geometry__(char* buf, WnckWindow* ww, char* x, char* y, 
 	sprintf(buf, "%d,%d,%d,%d", x1, y1, w1, h1);
 }
 
-static void z__(const string& code) {
+static void z__(const string& code, void* p1) {
 	int err;
-	const char* ret = cb_(jsq_, main_qu_, &err, NULL, code.c_str(), false, NULL, 0);
+	char s1[32];
+	sprintf(s1, "%lu", p1);
+	const char* ret = cb_(jsq_, main_qu_, &err, NULL, code.c_str(), false, NULL, 1, s1);
 	l4_err_out_(jsq_, ret, err, 1);
 }
 
 static string code_window_opened_;
 static void window_opened__(WnckScreen *screen, WnckWindow *window, gpointer user_data) {
-	z__(code_window_opened_);
+	z__(code_window_opened_, window);
 }
 dlle___ void on_window_opened__(const char*code, WnckScreen *screen) {
 	code_window_opened_ = code;
 	g_signal_connect (screen, "window-opened", G_CALLBACK (window_opened__), NULL);
 }
 
+static vector<WnckWindow *> v_name_changed_;
+
 static string code_window_closed_;
 static void window_closed__(WnckScreen *screen, WnckWindow *window, gpointer user_data) {
-	z__(code_window_closed_);
+	z__(code_window_closed_, window);
+	auto i = find(v_name_changed_.begin(), v_name_changed_.end(), window);
+	if(i != v_name_changed_.end())
+		v_name_changed_.erase(i);
 }
 dlle___ void on_window_closed__(const char*code, WnckScreen *screen) {
 	code_window_closed_ = code;
@@ -150,9 +160,23 @@ dlle___ void on_window_closed__(const char*code, WnckScreen *screen) {
 
 static string code_active_workspace_changed_;
 static void active_workspace_changed__(WnckScreen *screen, WnckWorkspace *previously_active_space, gpointer user_data) {
-	z__(code_active_workspace_changed_);
+	z__(code_active_workspace_changed_, previously_active_space);
 }
 dlle___ void on_active_workspace_changed__(const char*code, WnckScreen *screen) {
 	code_active_workspace_changed_ = code;
 	g_signal_connect (screen, "active-workspace-changed", G_CALLBACK (active_workspace_changed__), NULL);
+}
+
+static string code_name_changed_;
+static void name_changed__(WnckWindow *window, gpointer user_data) {
+	z__(code_name_changed_, window);
+}
+dlle___ void on_name_changed__(const char*code, WnckWindow *window) {
+	if(find(v_name_changed_.begin(), v_name_changed_.end(), window) != v_name_changed_.end()) {
+		return;
+	}
+	v_name_changed_.push_back(window);
+
+	code_name_changed_ = code;
+	g_signal_connect (window, "name-changed", G_CALLBACK (name_changed__), NULL);
 }
